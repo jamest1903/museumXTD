56e29d22fb896d56f779baf5603ec37c5fe94ec8
James Taylor <james.taylor@heig-vd.ch>
2022-07-25 16:15:49 +0200

    now calculating the parentFolder of md file when generating its link

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,49 @@
+const fs = require("fs")
+const path = require("path")
+let avoid=['.eleventy.js','.git','.gitignore','.obsidian','assets','css','dist','node_modules','tables','_data','_includes','package.json','package-lock.json'];
+
+const getAllFiles = function(dirPath, arrayOfFiles) {
+  let files = fs.readdirSync(dirPath)
+  var arrayOfFiles = arrayOfFiles || [];
+  
+  files.forEach(function(file) {
+    let skip=false;
+    avoid.forEach(function(f){
+      if(f==file)
+        skip=true;
+    })
+    if(!skip){
+      if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+        arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+      } else {
+        arrayOfFiles.push(path.join(dirPath, "/", file))
+      }
+    }
+  })
+  return arrayOfFiles
+}
+
 module.exports = config => {
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy('./images/');
   config.addPassthroughCopy("./css");
   config.addPassthroughCopy("./assets");
+
+  const all_files = getAllFiles(process.cwd());
+
+  const getFolder = function(fileName) {
+    for (const file of all_files) {
+      if(file.indexOf(fileName)>-1){
+        let filePath = file.split('\\');
+        let i = filePath.indexOf(fileName+'.md');
+        if(filePath[i-1] === 'museumXTD') {
+          return '';
+        }
+        return filePath[i-1];
+      }
+    }
+    return '';
+  }
   
   let markdownIt = require("markdown-it");
   //let markdownItObsidian = require("markdown-it-obsidian")();
@@ -18,10 +59,19 @@ module.exports = config => {
       const parts = match.raw.slice(2,-2).split("|");
       parts[0] = parts[0].replace(/.(md|markdown)\s?$/i, "");
       match.text = (parts[1] || parts[0]).trim();
-      match.url = `/Notes/${parts[0].trim()}/`; 
+      if(parts[0] === 'index'){
+        match.url = `/${parts[0].trim()}.html`;
+      } else {
+        const parentFolder = getFolder(parts[0]);
+        if(parentFolder) {
+          match.url = `/${parentFolder}/${parts[0].trim()}/`;
+        } else {
+          match.url = `/${parts[0].trim()}/`;
+        }
+      }
     }
   })
-    //.use(markdownItObsidian)
+  
   config.setLibrary("md", markdownLib);
 
   return {
